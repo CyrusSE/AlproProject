00959a4e591bf82f687fa436746470f0e8e7cf3e
Pirjakun <90229340+Pirjakun@users.noreply.github.com>
2024-05-09 00:42:10 +0700

    Patch 1.2

--- tubes.go
+++ tubes.go
@@ -4,16 +4,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-
-	"github.com/common-nighthawk/go-figure"
 )
 
 const NMAX int = 1000
 
 type dataAccount struct {
 	nama     string
-	password string
 	username string
+	password string
 	PIN      int
 }
 
@@ -23,16 +21,17 @@ var pilihan int
 
 func main() {
 	var account acc
-	main_menu(&account)
+	var totalAccount int
+	main_menu(&account, &totalAccount)
 	fmt.Print(account[0].nama)
 }
 
-func main_menu(akun *acc) {
-	menu("Selamat datang di aplikasi konsultasi online.", "1. Pasien", "2. Dokter", "3. Liat Forum Konsultasi", "4. Exit", "", "Silahkan pilih keperluan anda.", "MAIN", false, akun)
+func main_menu(akun *acc, T *int) {
+	menu("Selamat datang di aplikasi konsultasi online.", "1. Pasien", "2. Dokter", "3. Liat Forum Konsultasi", "4. Exit", "", "Silahkan pilih keperluan anda.", "MAIN", false, akun, *T)
 	fmt.Scan(&pilihan)
 	switch pilihan {
 	case 1:
-		pasien(*akun)
+		pasien(*akun, T, false)
 	case 2:
 		fmt.Println("SOON")
 	case 3:
@@ -40,31 +39,35 @@ func main_menu(akun *acc) {
 	case 4:
 		bye()
 	default:
-		menu("Masukkan angka sesuai option.", "1. Pasien", "2. Dokter", "3. Liat Forum Konsultasi", "4. Exit", "", "Silahkan pilih keperluan anda.", "MAIN", false, akun)
+		menu("Masukkan angka sesuai option.", "1. Pasien", "2. Dokter", "3. Liat Forum Konsultasi", "4. Exit", "", "Silahkan pilih keperluan anda.", "MAIN", false, akun, *T)
 		fmt.Scan(&pilihan)
 	}
 }
 
-func pasien(akun acc) {
-	menu("Selamat datang di menu pasien.", "1. Login Akun Pasien", "2. Buat Akun Baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &akun)
+func pasien(akun acc, T *int, hide bool) {
+	if !hide {
+		menu("Selamat datang di menu pasien.", "1. Login Akun Pasien", "2. Buat Akun Baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &akun, *T)
+	}
 	fmt.Scan(&pilihan)
 	switch pilihan {
 	case 1:
-		menu("", "Login Akun Pasien", "", "", "", "", "Silahkan masukkan username dan password anda.", "PASIEN", true, &akun)
-		loginAccount(akun)
+		menu("", "Login Akun Pasien", "", "", "", "", "Silahkan masukkan username dan password anda.", "PASIEN", true, &akun, *T)
+		loginAccount(akun, *T)
 	case 2:
-		menu("", "Buat Akun Baru Pasien", "", "", "", "", "Silahkan masukkan username dan password anda.", "PASIEN", false, &akun)
+		menu("", "Buat Akun Baru Pasien.", "", "", "", "", "Silahkan masukkan username dan password anda.", "PASIEN", true, &akun, *T)
+		makeAcc(&akun, T)
 	case 3:
-		//Recovery
+		menu("", "Recovery Akun Pasien.", "", "", "", "", "Masukkan username dan PIN anda.", "PASIEN", true, &akun, *T)
+		recovery(&akun, *T)
 	case 4:
-		main_menu(&akun)
+		main_menu(&akun, T)
 	default:
-		menu("Masukkan angka sesuai option.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &akun)
+		menu("Masukkan angka sesuai option.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &akun, *T)
 		fmt.Scan(&pilihan)
 	}
 }
 
-func menu(header, kata1, kata2, kata3, kata4, kata5, pesan, menuType string, hide bool, akun *acc) {
+func menu(header, kata1, kata2, kata3, kata4, kata5, pesan, menuType string, hide bool, akun *acc, T int) {
 	clear()
 	color("a")
 	title("TUGAS BESAR ALGORITMA PEMPROGRAMAN")
@@ -118,27 +121,97 @@ func menu(header, kata1, kata2, kata3, kata4, kata5, pesan, menuType string, hid
 	}
 }
 
-func loginAccount(A acc) {
+func loginAccount(A acc, T int) {
 	var username, password string
-	var found bool
+	var indexUser int
 	fmt.Printf("%3s %s", "", "Masukkan username Anda : ")
 	fmt.Scan(&username)
 	fmt.Printf("%3s %s", "", "Masukkan password Anda : ")
 	fmt.Scan(&password)
-	for i := 0; i < len(A); i++ {
-		if A[i].username == username && A[i].password == password {
-			//Login Berhasil
-			found = true
-			break
-		} else if A[i].username == username && A[i].password != password {
-			menu("Password anda salah, gunakan PIN untuk recovery akun anda.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &A)
-			found = true
-			break
+	if exist(A, T, username){
+		indexUser = findIndexUser(A, T, username)
+		if A[indexUser].username == username && A[indexUser].password == password {
+			//Login berhasil
+		} else {
+			menu("Password anda salah, gunakan PIN untuk recovery akun anda.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &A, T)
+			pasien(A, &T, true)
+		}
+	}else{
+		menu("Akun anda tidak ditemukan, silahkan buat akun baru.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &A, T)
+		pasien(A, &T, true)
+	}
+}
+
+
+func makeAcc(A *acc, T *int) {
+	var nama, username, password string
+	var pin int
+	fmt.Printf("%3s %s", "", "Masukkan nama Anda : ")
+	fmt.Scan(&nama)
+	fmt.Printf("%3s %s", "", "Masukkan username Anda : ")
+	fmt.Scan(&username)
+	fmt.Printf("%3s %s", "", "Masukkan password Anda : ")
+	fmt.Scan(&password)
+	fmt.Printf("%3s %s", "", "Masukkan PIN Anda (4 Digit) : ")
+	fmt.Scan(&pin)
+
+	if exist(*A,*T, username) {
+		menu("Username yang ada masukan telah di gunakan, silahkan gunakan username lain.", "Buat Akun Baru Pasien", "", "", "", "", "Silahkan masukkan username dan password anda.", "PASIEN", true, A, *T)
+		makeAcc(A, T)
+	} else {
+		A[*T].nama = nama
+		A[*T].username = username
+		A[*T].password = password
+		A[*T].PIN = pin
+		*T += 1
+		menu("Akun berhasil dibuat, Silahkan login menggunakan akun anda!", "1. Login Akun Pasien", "2. Buat Akun Baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, A, *T)
+		pasien(*A, T, true)
+	}
+}
+
+func exist(A acc, T int, usr string) bool {
+	for i := 0; i < T; i++ {
+		if usr == A[i].username {
+			return true
 		}
 	}
-	if !found {
-		menu("Akun anda tidak ditemukan, silahkan buat akun baru.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, &A)
+	return false
+}
+
+func recovery(A *acc, T int){
+	var username string
+	var passwordBaru string
+	var indexUser, pin int
+	fmt.Printf("%3s %s", "", "Masukkan username Anda : ")
+	fmt.Scan(&username)
+	if exist(*A, T, username) {
+		indexUser = findIndexUser(*A, T, username)
+		fmt.Printf("%3s %s", "", "Masukkan PIN baru Anda : ")
+		fmt.Scan(&pin)
+		
+		if A[indexUser].PIN == pin {
+			fmt.Printf("%3s %s", "", "Masukkan password baru Anda : ")
+			fmt.Scan(&passwordBaru)
+			A[indexUser].password = passwordBaru
+			menu("Akun anda berhasil di Recovery, silahkan login dengan password baru.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, A, T)
+			pasien(*A, &T, true)
+		}else{
+			menu("Anda memasukan PIN yang salah.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, A, T)
+			pasien(*A, &T, true)
+		}
+	}else{
+		menu("Akun anda tidak ditemukan, silahkan buat akun baru.", "1. Login Akun Pasien", "2. Buat Akun baru Pasien", "3. Recovery Akun Pasien", "4. Back", "", "Silahkan pilih keperluan anda.", "PASIEN", false, A, T)
+		pasien(*A, &T, true)
 	}
+}
+
+func findIndexUser(A acc,  T int, usr string) int{
+	for i := 0; i < T; i++ {
+		if usr == A[i].username {
+			return i
+		}
+	}
+	return 0
 }
 
 func clear() {
@@ -161,6 +234,9 @@ func title(text string) {
 
 func bye() {
 	clear()
-	myFigure := figure.NewFigure("GOOD BYE!", "", true)
-	myFigure.Print()
+	fmt.Println("  ____    ___     ___    ____      ____   __   __  _____   _")
+	fmt.Println(" / ___|  / _ \\   / _ \\  |  _ \\    | __ )  \\ \\ / / | ____| | |")
+	fmt.Println("| |  _  | | | | | | | | | | | |   |  _ \\   \\ V /  |  _|   | |")
+	fmt.Println("| |_| | | |_| | | |_| | | |_| |   | |_) |   | |   | |___  |_|")
+	fmt.Println(" \\____|  \\___/   \\___/  |____/    |____/    |_|   |_____| (_)")
 }
